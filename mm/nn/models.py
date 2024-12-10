b28163a14c7e6efa9645331bf72fd331225cf7e3
kr4nas <anastasiakrajnova61@gmail.com>
2024-12-10 14:06:12 +0300

    eee

--- mm/nn/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Schedule(models.Model):
-    group = models.CharField(max_length=10)
-    date = models.DateField()
-    content = models.TextField()
-
-    def __str__(self):
-        return f"{self.group} - {self.date}"
--- mm/nn/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ScheduleView
-
-urlpatterns = [
-    path('update_nn/<str:group>/<str:date>/', ScheduleView.as_view(), name='update_schedule'),
-    path('get_nn/<str:group>/<str:date>/', ScheduleView.as_view(), name='get_schedule'),
-]
